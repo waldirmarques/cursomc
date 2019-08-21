7c5872d1c11d054a7fd7d49ab56acb1b5c1d7141
WaldirMarques <waldirmarquesdesouza@gmail.com>
2019-08-21 12:59:26 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/br/waldir/CursomcApplication.java
+++ src/main/java/com/br/waldir/CursomcApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.br.waldir.domain.Categoria;
+import com.br.waldir.domain.Produto;
 import com.br.waldir.repositories.CategoriaRepository;
+import com.br.waldir.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
-	private CategoriaRepository repository;
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -25,7 +29,19 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null,"Informatica");
 		Categoria cat2 = new Categoria(null,"Escritorio");
 		
-		repository.saveAll(Arrays.asList(cat1, cat2));
+		Produto pro1 = new Produto (null,"Computador",2200.000);
+		Produto pro2 = new Produto (null,"Impressora",800.00);
+		Produto pro3 = new Produto (null,"Mause",15.0);
+		
+		cat1.getProtudos().addAll(Arrays.asList(pro1,pro2,pro3));
+		cat2.getProtudos().addAll(Arrays.asList(pro2));
+		
+		pro1.getCategorias().addAll(Arrays.asList(cat1));
+		pro2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		pro3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(pro1,pro2,pro3));
 		
 	}
 
