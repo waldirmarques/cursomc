c84bcb3547961dd0ae14f7d3f33c673a8f58f98a
WaldirMarques <waldirmarquesdesouza@gmail.com>
2019-08-20 23:30:16 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/br/waldir/resources/CategoriaResouces.java
+++ src/main/java/com/br/waldir/resources/CategoriaResouces.java
@@ -1,15 +1,27 @@
 package com.br.waldir.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.br.waldir.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResouces {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando plenamente";
+	public List<Categoria> listar() {
+		Categoria cat01 = new Categoria(1,"Informática");
+		Categoria cat02 = new Categoria(2,"Escritório");
+		
+		List <Categoria> lista = new ArrayList<>();
+		lista.add(cat01);
+		lista.add(cat02);
+		
+		return lista;
 	}
 }
