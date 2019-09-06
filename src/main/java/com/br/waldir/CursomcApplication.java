56002eabc432df92c7abf3ab18b7c342e1969447
WaldirMarques <waldirmarquesdesouza@gmail.com>
2019-09-05 22:58:14 -0300

    Pedido, EstadoPagamento e PagamentoComCartão

--- src/main/java/com/br/waldir/CursomcApplication.java
+++ src/main/java/com/br/waldir/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.br.waldir;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,29 +13,41 @@ import com.br.waldir.domain.Cidade;
 import com.br.waldir.domain.Cliente;
 import com.br.waldir.domain.Endereco;
 import com.br.waldir.domain.Estado;
+import com.br.waldir.domain.Pagamento;
+import com.br.waldir.domain.PagamentoComBoleto;
+import com.br.waldir.domain.PagamentoComCartao;
+import com.br.waldir.domain.Pedido;
 import com.br.waldir.domain.Produto;
+import com.br.waldir.domain.enums.EstadoPagamento;
 import com.br.waldir.domain.enums.TipoCliente;
 import com.br.waldir.repositories.CategoriaRepository;
 import com.br.waldir.repositories.CidadeRepository;
 import com.br.waldir.repositories.ClienteRepository;
 import com.br.waldir.repositories.EnderecoRepository;
 import com.br.waldir.repositories.EstadoRepository;
+import com.br.waldir.repositories.PagamentoRepository;
+import com.br.waldir.repositories.PedidoRepository;
 import com.br.waldir.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-	
-	@Autowired //Alto instancia a classe sem precisar do new.
+
+	@Autowired // Alto instancia a classe sem precisar do new.
 	private CategoriaRepository categoriaRepository;
-	@Autowired 
+	@Autowired
 	private ProdutoRepository produtoRepository;
-	@Autowired 
+	@Autowired
 	private EstadoRepository estadoRepository;
-	@Autowired 
+	@Autowired
 	private CidadeRepository cidadeRepository;
 	@Autowired
 	private ClienteRepository clienteRepository;
-	@Autowired EnderecoRepository enderecoRepository;
+	@Autowired
+	EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -78,6 +91,7 @@ public class CursomcApplication implements CommandLineRunner {
 		Cliente cli1 = new Cliente(null,"Waldir Marques","waldir.marques@dce.ufpb.br","4143532532",TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("634634414","674735634"));
 		
+		//Parou na aula 22
 		//Cliente cli2 = new Cliente(null,"Gustavo","gustavo.venezes@dce.ufpb.br","351423545",TipoCliente.PESSOAFISICA);
 		
 		Endereco e1 = new Endereco(null, "sertão", "313", "centro", "Perto a faculdade", "580000000",cli1,c1);
@@ -85,8 +99,24 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		cli1.getEndereco().addAll(Arrays.asList(e1,e2));
 		
-		clienteRepository.saveAll(Arrays.asList(cli1));
+		clienteRepository.saveAll(Arrays.asList(cli1)); 
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2,sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 
 }
