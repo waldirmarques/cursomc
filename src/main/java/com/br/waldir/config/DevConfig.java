eb2f9c1c31ba7478af19b2f1474cf463a11da3c5
WaldirMarques <waldirmarquesdesouza@gmail.com>
2019-09-10 10:18:27 -0300

    criação da class DevConfig e melhorias no application

--- src/main/java/com/br/waldir/config/DevConfig.java
+++ src/main/java/com/br/waldir/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.br.waldir.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,11 +16,16 @@ public class DevConfig {
 	
 	@Autowired
 	private DBService dbService;
+	
+	@Value("$(spring.jpa.hibernate.ddl-auto)")
+	private String strategy;
+	
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
-		
+		if (!"create".equals(strategy)) {
+			return false;
+		}
 		dbService.instantiateTestDatabase();
-		
 		return true;
 	}
 }
