07c603e2972c83b68233ac47ba606e725d815e5e
WaldirMarques <waldirmarquesdesouza@gmail.com>
2019-09-10 02:13:52 -0300

    criando profile de desenvolvimento

---
+++ src/main/java/com/br/waldir/config/DevConfig.java
@@ -0,0 +1,25 @@
+package com.br.waldir.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.br.waldir.servives.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		dbService.instantiateTestDatabase();
+		
+		return true;
+	}
+}
